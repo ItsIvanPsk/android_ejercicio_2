93adc911e378d1491743cf5f5fc40eb6c55cdb51
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2022-09-16 19:20:29 +0200

    Ejercicio2

--- app/src/main/java/com/example/adivinaelnumero/MainActivity.java
+++ app/src/main/java/com/example/adivinaelnumero/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.adivinaelnumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int secretNum = genRandomNumber();
+
+        Button checkBtn = findViewById(R.id.checkBtn);
+        EditText userNumber = findViewById(R.id.inputNumber);
+
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context con = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+
+                int userNum = Integer.parseInt(userNumber.getText().toString());
+                if (secretNum == userNum)
+                {
+                    System.out.println("Equal!");
+                    CharSequence txt = "You WIN!!";
+                    showToast(con, txt, duration);
+                }
+                else if (userNum < secretNum)
+                {
+                    System.out.println("Less!");
+                    CharSequence txt = "The number is higher than " + userNumber.getText();
+                    showToast(con, txt, duration);
+                }
+                else
+                {
+                    System.out.println("Higher!");
+                    CharSequence txt = "The number is lower than " + userNumber.getText();
+                    showToast(con, txt, duration);
+                }
+            }
+        });
+    }
+
+    private void showToast(Context con, CharSequence txt, int duration)
+    {
+        Toast t = Toast.makeText(con,txt,duration);
+        t.show();
+    }
+
+    private int genRandomNumber()
+    {
+        return (int)(Math.random() * 10);
     }
 }
